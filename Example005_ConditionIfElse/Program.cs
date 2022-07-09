a272703fa58d1b6e979aef0856061380037158d7
rustam <rusitama@gmail.com>
2022-07-09 18:27:06 +0500

    Add Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿string username = Console.ReadLine();
+
+if(username.ToLower() == "masha"){
+    Console.WriteLine("Ura this is masha!");
+}
+else
+{
+    Console.Write("Privet, ");
+    Console.WriteLine(username);
+}
