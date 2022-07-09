ae75f2bbd18d76e2daf4800792ef9e7e6fd23f85
rustam <rusitama@gmail.com>
2022-07-09 18:40:49 +0500

    Add example 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = 2, b = 3, c = 5, d = 8, e = 4;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
