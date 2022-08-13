b5cad5fe4679ccaba129e511182c71563d045e62
rustam <rusitama@gmail.com>
2022-08-13 18:02:49 +0500

    Add exmample 013 Fibonacci

---
+++ Example013_Fibonacci/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// f(1) = 1
+// f(2) = 2
+// f(n) = f(n - 1) + f(n - 2)
+
+int Fibonnaci(int n){
+    if (n == 1 || n == 2) return n;
+    else return Fibonnaci(n - 1) + Fibonnaci(n - 2);
+}
+
+for (int i = 1; i < 50; i++) 
+{
+    Console.WriteLine($"f({i}) = {Fibonnaci(i)}");
+}
