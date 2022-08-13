1992b7c5b75b831bc925be625a948660d3600697
rustam <rusitama@gmail.com>
2022-08-13 18:02:01 +0500

    Add exmample 013 Factorial

---
+++ Example013_Factorial/Program.cs
@@ -0,0 +1,12 @@
+﻿double Factor(int n)
+{
+    // 1! = 1;
+    // 0! = 1;
+    if (n == 1) return n;
+    else return n * Factor(n - 1);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"{i}! = {Factor(i)}");
+}
